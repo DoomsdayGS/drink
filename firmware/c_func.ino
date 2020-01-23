ebb29c1e1599d4219b754a250fdb2570e5e01ae4
DoomsdayGS <g99731569@gmail.com>
2020-01-23 15:55:40 +0300

    1.3
       Версия 1.3:
       - Исправлен баг со снятием рюмки в авто режиме (жука поймал Юрий Соколов)

--- firmware/c_func.ino
+++ firmware/c_func.ino
@@ -92,13 +92,11 @@ void flowTick() {
         strip.setLED(i, mCOLOR(BLACK));                             // нигра
         LEDchanged = true;
         timeoutReset();                                             // сброс таймаута
-        if (systemState == PUMPING) {                               // убрали во время заправки!
-          if (i == curPumping) {
-            curPumping = -1; // снимаем выбор рюмки
-            systemState = WAIT;                                         // режим работы - ждать
-            WAITtimer.reset();
-            pumpOFF();                                                  // помпу выкл
-          }
+        if (i == curPumping) {
+          curPumping = -1; // снимаем выбор рюмки
+          systemState = WAIT;                                         // режим работы - ждать
+          WAITtimer.reset();
+          pumpOFF();                                                  // помпу выкл
         }
         DEBUG("take glass");
         DEBUG(i);
